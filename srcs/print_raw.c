24130cf009618ba26b47251e791e9249f19706a6
Martinho23 <ajoni844@gmail.com>
2018-03-19 19:54:48 +0100

    add print_raw.c and getprecision.c

---
+++ srcs/print_raw.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_raw.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jfarinha <jfarinha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/10 11:22:37 by jfarinha          #+#    #+#             */
+/*   Updated: 2018/03/10 11:33:30 by jfarinha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/project.h"
+#include <unistd.h>
+
+static int	print_raw(const char *format, t_format_data *data)
+{
+	int		i;
+
+	i = 0;
+	while (format[data->index + i] && format[data->index + i] != '%')
+		i++;
+	write(1, &format[data->index], i);
+	data->index + i;
+	return (i);
+}
