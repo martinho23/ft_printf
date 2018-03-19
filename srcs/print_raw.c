8c5b216e033049c907ee7a5a620dada3cff210c0
Martinho23 <ajoni844@gmail.com>
2018-03-19 20:05:12 +0100

    fixed bug were print_raw din't send the right amount of printed chars

--- srcs/print_raw.c
+++ srcs/print_raw.c
@@ -6,7 +6,7 @@
 /*   By: jfarinha <jfarinha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/10 11:22:37 by jfarinha          #+#    #+#             */
-/*   Updated: 2018/03/10 11:33:30 by jfarinha         ###   ########.fr       */
+/*   Updated: 2018/03/19 20:04:05 by jfarinha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,6 @@ static int	print_raw(const char *format, t_format_data *data)
 	while (format[data->index + i] && format[data->index + i] != '%')
 		i++;
 	write(1, &format[data->index], i);
-	data->index + i;
+	data->index += i;
 	return (i);
 }
