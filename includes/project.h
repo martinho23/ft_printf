b1f108b68b28e9c336470cecbb225922fcc1cf6a
Martinho23 <ajoni844@gmail.com>
2018-03-28 13:20:52 +0200

    LAst version of printf

--- includes/project.h
+++ includes/project.h
@@ -6,7 +6,7 @@
 /*   By: jfarinha <jfarinha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/17 08:23:02 by jfarinha          #+#    #+#             */
-/*   Updated: 2018/03/09 13:29:33 by jfarinha         ###   ########.fr       */
+/*   Updated: 2018/03/10 11:24:40 by jfarinha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ typedef struct	s_format_data
 }				t_format_data;
 
 int				ft_printf(const char *format, ...);
+static int		print_raw(const char *format, t_format_data *data);
 static void		clean_flags(t_format_data *data);
 static void		getflags(const char *format, t_format_data *data);
 static void		process_flags(const char *format, t_format_data *data);
--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,9 +6,30 @@
 /*   By: jfarinha <jfarinha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/26 16:17:36 by jfarinha          #+#    #+#             */
-/*   Updated: 2018/01/26 16:53:42 by jfarinha         ###   ########.fr       */
+/*   Updated: 2018/03/10 11:33:03 by jfarinha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/project.h"
+#include "../libft/libft.h"
 
+int		ft_printf(const char *format, ...)
+{
+	t_format_data	data;
+	int				count;
+
+	data.index = 0;
+	count = 0;
+	while (format[data.index])
+	{
+		if (format[data.index++] == '%' && format[data.index] != '%')
+		{
+			getflags(format, &data);
+			getwidth(format, &data);
+			getprecision(format, &data);
+		}
+		else
+			count += print_raw(format, &data);
+	}
+	return (count);
+}
