1b1caa64d3948be5e6134e9f6ebaad02e8d6d0a9
Martinho23 <ajoni844@gmail.com>
2018-07-07 16:21:42 +0200

    Addaded Null char support on chars_handler.c

--- srcs/chars_handler.c
+++ srcs/chars_handler.c
@@ -6,31 +6,28 @@
 /*   By: jfarinha <jfarinha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/21 14:22:41 by jfarinha          #+#    #+#             */
-/*   Updated: 2018/07/01 10:22:34 by jfarinha         ###   ########.fr       */
+/*   Updated: 2018/07/07 16:20:17 by jfarinha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
-static int	init_char_handler(t_fdata *data, int *padding)
+static int	init_char_handler(t_fdata *data, int *padding, int c)
 {
 	int		cnbr;
 
 	cnbr = (data->fwidth > 1) ? data->fwidth : 1;
-	*padding = data->fwidth - 1;
-	if (data->preci != (-1))
-		return (-1);
-	if (data->flags[0])
-		return (-1);
-	if (data->flags[1])
-		return (-1);
-	if (data->flags[2])
-		return (-1);
-	if (data->flags[4])
-		return (-1);
-	if (data->len != 2 && data->len != (-1))
-		return (-1);
+	*padding = data->fwidth;
+	*padding -= (c == 0) ? 2 : 1;
 	return (cnbr);
+}
+
+int			process(int c, int fd)
+{
+	if (c == 0)
+		return (ft_putstr_fd("^@", fd));
+	else
+		return (ft_putchar_fd(c, fd));
 }
 
 int			char_handler(const char *format, t_fdata *data, va_list *ap)
@@ -39,15 +36,15 @@ int			char_handler(const char *format, t_fdata *data, va_list *ap)
 	int		padding;
 	int		c;
 
-	len = init_char_handler(data, &padding);
 	if (data->len == 2 || format[data->index] == 'C')
 		c = va_arg(*ap, wchar_t);
 	else
 		c = va_arg(*ap, int);
 	data->index++;
+	len = init_char_handler(data, &padding, c);
 	if (len == (-1))
 		return (-1);
-	(!data->flags[3]) ? pad(padding, ' ') : ft_putchar_fd(c, 1);
-	(data->flags[3]) ? pad(padding, ' ') : ft_putchar_fd(c, 1);
+	len = (!data->flags[3]) ? pad(padding, ' ') : process(c, 1);
+	len += (data->flags[3]) ? pad(padding, ' ') : process(c, 1);
 	return (len);
 }
